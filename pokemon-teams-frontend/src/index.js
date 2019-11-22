5363114802f3e9df145435f9c0da47963b9965b9
Hallie Dunn <hal.dunn@flatironschool.com>
2019-11-22 14:07:20 -0800

    fiddling with the backend

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,15 @@
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "http://127.0.0.1:3000/"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(TRAINERS_URL)
+    .then(res=>res.json())
+    .then(json =>{
+        renderTrainers(json)
+    })
+
+    function renderTrainers(trainers){
+        console.log(trainers)
+    }
+})
